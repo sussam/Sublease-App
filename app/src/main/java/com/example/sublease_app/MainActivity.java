ddee6044ac58f25db4d4294e2bd78b5559eb19e1
samanthasu <sus@ufl.edu>
2020-03-30 13:28:28 -0400

    App Created

---
+++ app/src/main/java/com/example/sublease_app/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.sublease_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toast.makeText(MainActivity.this, "firebase connection success", Toast.LENGTH_LONG).show();
+    }
+}
